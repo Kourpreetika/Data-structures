9ed5f7a54b6bccbb5b0082baff70f4070762bb30
Kourpreetika <kourpretika@gmail.com>
2026-03-19 18:16:22 +0530

    lemonade change

---
+++ greedy/partition array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& arr) {
+        int total = 0;
+        for(int x : arr)
+        total += x;
+        if(total % 3 != 0)
+        return false;
+        int target = total / 3;
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i< arr.size();i++){
+            sum += arr[i];
+            if(sum == target){
+                count ++;
+                sum = 0;
+            }
+        }
+        return count >= 3;
+    }
+};
+//https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/?envType=problem-list-v2&envId=greedy
